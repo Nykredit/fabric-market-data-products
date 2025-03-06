ef8f421ba486d5cbc594a3f06da8d37787abae50
Rasmus F. Larsen <yil8@nykredit.dk>
2025-03-06 11:07:15 +0100

    Test

--- fabric/DMS_SJ_TestLib.SparkJobDefinition/Libs/DMS_SJ_Library.py
+++ fabric/DMS_SJ_TestLib.SparkJobDefinition/Libs/DMS_SJ_Library.py
@@ -1,2 +1,16 @@
+import notebookutils
 def print_hello_world():
-    print("Abc")
+    # Get raw lakehouse data
+    lakehouses = notebookutils.lakehouse.list()
+
+    # Extract paths
+    paths = {lh['displayName'].split('_')[-1].lower(): lh['properties']['abfsPath'] for lh in lakehouses}
+
+    # Assign variables
+    local_bronze_path = paths.get('bronze', '')
+    local_silver_path = paths.get('silver', '')
+    local_gold_path = paths.get('gold', '')
+
+    print(local_bronze_path)
+    print(local_silver_path)
+    print(local_gold_path)
--- fabric/DMS_SJ_TestLib.SparkJobDefinition/Main/DMS_SJ_Test.py
+++ fabric/DMS_SJ_TestLib.SparkJobDefinition/Main/DMS_SJ_Test.py
@@ -3,8 +3,7 @@ from DMS_SJ_Library import print_hello_world
 
 if __name__ == "__main__":
 
-    spark = (SparkSession.builder.appName("DMS_SJ_Bronze_Ingestion").getOrCreate())
+    spark = (SparkSession.builder.appName("DMS_SJ_Test").getOrCreate())
     spark_context = spark.sparkContext
 
-    print("Hello World")
     print_hello_world()
