37a26ff6085673f5e8700a117b6b428a0513085d
Rasmus F. Larsen <yil8@nykredit.dk>
2025-03-06 15:34:21 +0100

    Remote test spark job definition

--- fabric/DMS_SJ_TestLib.SparkJobDefinition/Main/DMS_SJ_Test.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-from DMS_SJ_Library import mount_lakehouses
-
-if __name__ == "__main__":
-
-    spark = (SparkSession.builder.appName("DMS_SJ_Test").getOrCreate())
-    spark_context = spark.sparkContext
-
-    mount_lakehouses()
