4ba365e29f0da5dcdaaf735f7e81708afcbb9527
Rasmus F. Larsen <yil8@nykredit.dk>
2025-03-06 10:48:08 +0100

    test

--- fabric/DMS_SJ_TestLib.SparkJobDefinition/Main/DMS_SJ_Test.py
+++ fabric/DMS_SJ_TestLib.SparkJobDefinition/Main/DMS_SJ_Test.py
@@ -1,3 +1,10 @@
+from pyspark.sql import SparkSession
+from DMS_SJ_Library import print_hello_world
+
 if __name__ == "__main__":
+
+    spark = (SparkSession.builder.appName("DMS_SJ_Bronze_Ingestion").getOrCreate())
+    spark_context = spark.sparkContext
+
     print("Hello World")
     print_hello_world()
