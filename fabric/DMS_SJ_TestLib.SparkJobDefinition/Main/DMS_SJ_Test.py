00ed4c1a49638be548d9ae61021cca0e7050064a
yil8-rasmus <yil8@nykredit.dk>
2025-03-06 10:25:47 +0100

    Committing 1 item from workspace 85ea3964-8cda-4b98-a90e-2bcd8a96d210

--- fabric/DMS_SJ_TestLib.SparkJobDefinition/Main/DMS_SJ_Test.py
+++ fabric/DMS_SJ_TestLib.SparkJobDefinition/Main/DMS_SJ_Test.py
@@ -1,5 +1,22 @@
-from utils.test import print_hello
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from datetime import datetime as dt
+from notebookutils import mssparkutils
+
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+shared_path = os.path.abspath(os.path.join(current_dir, "..", "..", "shared"))
+sys.path.append(shared_path)
+
+import test
+
 
 if __name__ == "__main__":
-    print("Hello world")
-    print_hello()
+
+    # Spark session builder, appName appears in log entries
+    spark = (SparkSession.builder.appName("DMS_SJ_Test").getOrCreate())
+    spark_context = spark.sparkContext
+    
+    test.hello_world()
