51c0ac5def0e8aa8856ba557b3693e990cbcee2e
Rasmus F. Larsen <yil8@nykredit.dk>
2025-03-06 11:15:00 +0100

    Mount lakehouses

--- fabric/DMS_SJ_TestLib.SparkJobDefinition/Libs/DMS_SJ_Library.py
+++ fabric/DMS_SJ_TestLib.SparkJobDefinition/Libs/DMS_SJ_Library.py
@@ -1,16 +1,26 @@
 import notebookutils
-def print_hello_world():
+
+def mount_lakehouses():
+    # Unmount all existing mounts
+    existing_mounts = notebookutils.mssparkutils.fs.ls('/mnt/')
+    for mount in existing_mounts:
+        notebookutils.mssparkutils.fs.unmount(mount.path)
+    
     # Get raw lakehouse data
     lakehouses = notebookutils.lakehouse.list()
 
     # Extract paths
     paths = {lh['displayName'].split('_')[-1].lower(): lh['properties']['abfsPath'] for lh in lakehouses}
 
-    # Assign variables
-    local_bronze_path = paths.get('bronze', '')
-    local_silver_path = paths.get('silver', '')
-    local_gold_path = paths.get('gold', '')
+    # Assign and mount lakehouses
+    if 'bronze' in paths:
+        notebookutils.mssparkutils.fs.mount(paths['bronze'], '/mnt/bronze')
+    if 'silver' in paths:
+        notebookutils.mssparkutils.fs.mount(paths['silver'], '/mnt/silver')
+    if 'gold' in paths:
+        notebookutils.mssparkutils.fs.mount(paths['gold'], '/mnt/gold')
 
-    print(local_bronze_path)
-    print(local_silver_path)
-    print(local_gold_path)
+    print("Lakehouses mounted successfully:")
+    print("Bronze:", paths.get('bronze', 'Not found'))
+    print("Silver:", paths.get('silver', 'Not found'))
+    print("Gold:", paths.get('gold', 'Not found'))
--- fabric/DMS_SJ_TestLib.SparkJobDefinition/Main/DMS_SJ_Test.py
+++ fabric/DMS_SJ_TestLib.SparkJobDefinition/Main/DMS_SJ_Test.py
@@ -1,9 +1,9 @@
 from pyspark.sql import SparkSession
-from DMS_SJ_Library import print_hello_world
+from DMS_SJ_Library import mount_lakehouses
 
 if __name__ == "__main__":
 
     spark = (SparkSession.builder.appName("DMS_SJ_Test").getOrCreate())
     spark_context = spark.sparkContext
 
-    print_hello_world()
+    mount_lakehouses()
