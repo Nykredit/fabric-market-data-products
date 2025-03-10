a99af476b6c23910b15928003fefc258a40c6d26
Alexander Andreasen <YKXC@nykredit.dk>
2025-03-10 17:43:41 +0100

    Updated the library to handle lakehouses

--- fabric/DMS_SJ_Bronze_Ingestion.SparkJobDefinition/Libs/shared_libs.py
+++ fabric/DMS_SJ_Bronze_Ingestion.SparkJobDefinition/Libs/shared_libs.py
@@ -1,14 +1,17 @@
-from pyspark.sql import SparkSession
-from abc import ABC, abstractmethod
 import logging
+
+from pyspark.sql import SparkSession
+from py4j.protocol import Py4JJavaError
+from abc import ABC, abstractmethod
+import notebookutils
 
 
 class SparkJob(ABC):
     """
     Abstract base class for Spark jobs.
 
-    This class provides a template for creating Spark jobs with a standardized setup_spark
-    for Spark sessions and logging.
+    This class provides a template for creating Spark jobs with a standardized setup for
+    Spark sessions and logging.
 
     Attributes
     ----------
@@ -16,51 +19,228 @@ class SparkJob(ABC):
         The Spark session for the job.
     logger : org.apache.log4j.Logger
         The logger for the job.
+    job_name : str
+        The name of the job.
 
     Methods
     -------
-    setup_spark(job_name: str)
-        Sets up the Spark session and logger.
+    setup_spark()
+        Sets up the Spark session.
+    setup_logger()
+        Sets up the logger.
     main()
         Abstract method to be implemented by subclasses with the main job logic.
     """
 
-    def setup_spark(self, job_name: str = None):
+    @property
+    def job_name(self) -> str:
         """
-        Sets up the Spark session and logger. 'job_name' is used to display in logging.
+        This property returns the name of the job, which defaults to the job class name.
 
-        Parameters
-        ----------
-        job_name : str
-            The name of the Spark job.
+        Returns
+        -------
+        str
+            The name of the job.
         """
-        if job_name is None:
-            job_name = self.__class__.__name__
-        self.job_name = job_name
+        return self.__class__.__name__
 
-        # Spark session builders
-        self.spark = (SparkSession.builder.appName(job_name).getOrCreate())
-        spark_context = self.spark.sparkContext
+    @property
+    def spark(self) -> SparkSession:
+        """
+        This property returns the Spark session for the job. If the Spark session has not
+        been initialized, it calls setup_spark() to initialize it.
 
-        log4jLogger = spark_context._jvm.org.apache.log4j
-        self.logger = log4jLogger.LogManager.getLogger(job_name)
-    
-        # Configure Python logging
+        Returns
+        -------
+        pyspark.sql.SparkSession
+            The Spark session for the job.
+        """
+        if not hasattr(self, "_spark"):
+            self.setup_spark()
+        return self._spark
+
+    @property
+    def logger(self) -> logging.Logger:
+        """
+        This property returns the logger for the job. If the logger has not been initialized,
+        it calls setup_logger() to initialize it.
+
+        Returns
+        -------
+        logging.Logger
+            The logger for the job.
+        """
+        if not hasattr(self, "_logger"):
+            self.setup_logger()
+        return self._logger
+
+    def setup_spark(self):
+        """
+        This method initializes the Spark session for the job.
+        """
+        self._spark = SparkSession.builder.appName(self.job_name).getOrCreate()
+
+    def setup_logger(self):
+        """
+        This method initializes the logger for the job to write to std:out.
+        """
         logging.basicConfig(
             level=logging.INFO,
             format="%(asctime)s %(levelname)s %(message)s",
             handlers=[logging.StreamHandler()]
         )
-        self.logger = logging.getLogger(job_name)
-
-
-        self.logger.info(f"Started sparksession and logging for job '{job_name}'.")
+        self._logger = logging.getLogger(self.job_name)
+        self._logger.info(f"Started sparksession and logging for job '{self.job_name}'.")
 
     @abstractmethod
     def main(self):
         """
-        Main method to be implemented by subclasses with the job logic.
-
-        This method should contain the main logic of the Spark job.
+        This abstract method should be implemented by subclasses to contain the main logic
+        of the Spark job.
         """
         pass
+
+
+class LakehouseError(Exception):
+    """Custom exception for lakehouse-related errors."""
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+
+class LakehouseUtils:
+    """
+    Utility class for managing lakehouses in Microsoft Fabric.
+
+    Methods
+    -------
+    get_current_workspace_lakehouse_names()
+        Retrieves the names of lakehouses in the current workspace.
+    get_lakehouse_path(lakehouse_name, workspace_id=None)
+        Retrieves the path of a lakehouse by name and optionally workspace ID.
+    get_bronze_lakehouse_path(workspace_id=None)
+        Retrieves the path of the bronze lakehouse in the current or specified workspace.
+    get_silver_lakehouse_path(workspace_id=None)
+        Retrieves the path of the silver lakehouse in the current or specified workspace.
+    get_gold_lakehouse_path(workspace_id=None)
+        Retrieves the path of the gold lakehouse in the current or specified workspace.
+    """
+
+    @staticmethod
+    def get_current_workspace_lakehouse_names() -> list[str]:
+        """
+        Retrieves the names of lakehouses in the current workspace.
+
+        Returns
+        -------
+        list[str]
+            A list of lakehouse names in the current workspace.
+        """
+        return [item["displayName"] for item in notebookutils.lakehouse.list()]
+
+    @staticmethod
+    def get_lakehouse_path(lakehouse_name: str, workspace_id: str = None) -> str:
+        """
+        Retrieves the path of a lakehouse by name and optionally workspace ID.
+
+        Parameters
+        ----------
+        lakehouse_name : str
+            The name of the lakehouse.
+        workspace_id : str, optional
+            The ID of the workspace.
+
+        Returns
+        -------
+        str
+            The path of the lakehouse.
+
+        Raises
+        ------
+        LakehouseError
+            If the lakehouse cannot be retrieved.
+        """
+        try:
+            if workspace_id:
+                lakehouse_item = notebookutils.lakehouse.get(lakehouse_name, workspace_id)
+            else:
+                lakehouse_item = notebookutils.lakehouse.get(lakehouse_name)
+        except Py4JJavaError as e:
+            raise LakehouseError(f"Failed to retrieve lakehouse '{lakehouse_name}': {e}")
+
+        return lakehouse_item["properties"]["abfsPath"]
+
+    @staticmethod
+    def get_bronze_lakehouse_path(workspace_id: str = None) -> str:
+        """
+        Retrieves the path of the bronze lakehouse in the current or specified workspace.
+
+        Parameters
+        ----------
+        workspace_id : str, optional
+            The ID of the workspace.
+
+        Returns
+        -------
+        str
+            The path of the bronze lakehouse.
+
+        Raises
+        ------
+        LakehouseError
+            If the bronze lakehouse cannot be found.
+        """
+        lakehouse_name = next((name for name in LakehouseUtils.get_current_workspace_lakehouse_names() if "bronze" in name), None)
+        if lakehouse_name is None:
+            raise LakehouseError("Bronze lakehouse not found in the current workspace.")
+        return LakehouseUtils.get_lakehouse_path(lakehouse_name, workspace_id)
+
+    @staticmethod
+    def get_silver_lakehouse_path(workspace_id: str = None) -> str:
+        """
+        Retrieves the path of the silver lakehouse in the current or specified workspace.
+
+        Parameters
+        ----------
+        workspace_id : str, optional
+            The ID of the workspace.
+
+        Returns
+        -------
+        str
+            The path of the silver lakehouse.
+
+        Raises
+        ------
+        LakehouseError
+            If the silver lakehouse cannot be found.
+        """
+        lakehouse_name = next((name for name in LakehouseUtils.get_current_workspace_lakehouse_names() if "silver" in name), None)
+        if lakehouse_name is None:
+            raise LakehouseError("Silver lakehouse not found in the current workspace.")
+        return LakehouseUtils.get_lakehouse_path(lakehouse_name, workspace_id)
+
+    @staticmethod
+    def get_gold_lakehouse_path(workspace_id: str = None) -> str:
+        """
+        Retrieves the path of the gold lakehouse in the current or specified workspace.
+
+        Parameters
+        ----------
+        workspace_id : str, optional
+            The ID of the workspace.
+
+        Returns
+        -------
+        str
+            The path of the gold lakehouse.
+
+        Raises
+        ------
+        LakehouseError
+            If the gold lakehouse cannot be found.
+        """
+        lakehouse_name = next((name for name in LakehouseUtils.get_current_workspace_lakehouse_names() if "gold" in name), None)
+        if lakehouse_name is None:
+            raise LakehouseError("Gold lakehouse not found in the current workspace.")
+        return LakehouseUtils.get_lakehouse_path(lakehouse_name, workspace_id)
--- fabric/DMS_SJ_Bronze_Ingestion.SparkJobDefinition/Main/DMS_SJ_Bronze_Ingestion.py
+++ fabric/DMS_SJ_Bronze_Ingestion.SparkJobDefinition/Main/DMS_SJ_Bronze_Ingestion.py
@@ -1,40 +1,28 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
 from datetime import datetime as dt
 import pytz
 import json
+
+from pyspark.sql.functions import col
 from notebookutils import mssparkutils
 
-if __name__ == "__main__":
+from shared_libs import SparkJob
 
-    # Spark session builder, appName appears in log entries
-    spark = (SparkSession.builder.appName("DMS_SJ_Bronze_Ingestion").getOrCreate())
-    spark_context = spark.sparkContext
 
-    # Setup logging
-    log4jLogger = spark_context._jvm.org.apache.log4j
-    logger = log4jLogger.LogManager.getLogger(spark.conf.get("spark.app.name"))
-    logger.setLevel(log4jLogger.Level.INFO)
+class BronzeIngestionJob(SparkJob):
 
-    sharedKey = mssparkutils.fs.head(f"Files/_meta/EventHubConnection.txt")
-    endpoint = "sb://nywt-dms-tst-function-eventhub.servicebus.windows.net/"
-    event_hub_name = "nywt-dms-tst-function-dms"
+    def __init__(self):
+        self.output_base_path = "Files/dms/"
 
-    event_hub_connection_string = f"Endpoint={endpoint};SharedAccessKeyName=ListenSharedAccessKey;SharedAccessKey={sharedKey};EntityPath={event_hub_name}"
+    def setup_event_stream(self):
+        pass
 
-    eh_conf = {
-        "eventhubs.connectionString": spark_context._jvm.org.apache.spark.eventhubs.EventHubsUtils.encrypt(event_hub_connection_string),    
-    }
-
-    output_base_path = "Files/dms/"
-
-    def write_to_file(df, epoch_id):
+    def write_to_file(self, df, epoch_id):
         try:
             if df.isEmpty():
-                logger.info(f"No data received in batch {epoch_id}. Skipping save.")
+                self.logger.info(f"No data received in batch {epoch_id}. Skipping save.")
                 return
 
-            logger.debug(f"Processing batch {epoch_id}")
+            self.logger.debug(f"Processing batch {epoch_id}")
 
             # Convert DataFrame to Pandas for row-wise iteration
             pdf = df.select("decoded_body", "sequenceNumber", "partition").toPandas()
@@ -47,30 +35,49 @@ if __name__ == "__main__":
 
                 event_type = json_content.get("Type", "unknown").lower()
 
-                partition_path = f"{output_base_path}{event_type}/{now.year}/{now.month:02d}/{now.day:02d}/"
+                partition_path = f"{self.output_base_path}{event_type}/{now.year}/{now.month:02d}/{now.day:02d}/"
                 output_path = f"{partition_path}event_{row['partition']}_{row['sequenceNumber']}.json"
 
                 json_data = json.dumps(json_content, indent=2)
 
                 mssparkutils.fs.put(output_path, json_data, overwrite=True)
-                logger.info(f"Saved event to file {output_path}")
-                print(f"Saved event to file {output_path}")
+                self.logger.info(f"Saved event to file {output_path}")
+
         except Exception as e:
-            logger.error(f"Error processing batch {epoch_id}: {e}")
-            print(f"ERROR: Processing batch {epoch_id}: {e}")
+            self.logger.error(f"Error processing batch {epoch_id}: {e}")
 
-    # Read from Event Hub
-    df_stream = (spark.readStream
-        .format("eventhubs")
-        .options(**eh_conf)    
-        .load()
-    )
+    def main(self):
 
-    df_stream = df_stream.select(
-        col("body").cast("string").alias("decoded_body"),    
-        col("partition"),
-        col("sequenceNumber"),    
-    )
+        sharedKey = mssparkutils.fs.head(f"Files/_meta/EventHubConnection.txt")
+        endpoint = "sb://nywt-dms-tst-function-eventhub.servicebus.windows.net/"
+        event_hub_name = "nywt-dms-tst-function-dms"
 
-    started_stream = df_stream.writeStream.foreachBatch(write_to_file).trigger(processingTime="5 minutes").start()
-    started_stream.awaitTermination()
+        event_hub_connection_string = f"Endpoint={endpoint};SharedAccessKeyName=ListenSharedAccessKey;SharedAccessKey={sharedKey};EntityPath={event_hub_name}"
+        eh_conf = {
+            "eventhubs.connectionString": self.spark.sc._jvm.org.apache.spark.eventhubs.EventHubsUtils.encrypt(event_hub_connection_string),    
+        }
+
+        # Read from Event Hub
+        df_stream = (self.spark.readStream
+            .format("eventhubs")
+            .options(**eh_conf)    
+            .load()
+        )
+
+        df_stream = df_stream.select(
+            col("body").cast("string").alias("decoded_body"),    
+            col("partition"),
+            col("sequenceNumber"),    
+        )
+
+        started_stream = (
+            df_stream.writeStream
+            .foreachBatch(self.write_to_file)
+            .trigger(processingTime="5 minutes")
+            .start()
+        )
+        started_stream.awaitTermination()
+
+
+if __name__ == "__main__":
+    BronzeIngestionJob().main()
